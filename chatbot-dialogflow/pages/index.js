9cf45d66e2fe1fe0a143da043083ec8b1728182c
ramit21 <21.ramit@gmail.com>
2022-02-10 23:18:04 +0530

    chatbot

--- chatbot-dialogflow/pages/index.js
+++ chatbot-dialogflow/pages/index.js
@@ -2,10 +2,6 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import KommunicateChat from './chat'
 
-import Kommunicate from '@kommunicate/kommunicate-chatbot-plugin';
-
-//Kommunicate.init("59efaeae43ebc6ee3803dc79f4e22c6d" , {"popupWidget":"true","automaticChatOpenOnNavigation":true})
-
 export default function Home() {
   return (
     <div className={styles.container}> Hello
